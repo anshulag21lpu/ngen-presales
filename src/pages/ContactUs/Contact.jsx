ed7035ccf824301c1dde214cab9f476806420d3b
ntyagi87 <90576735+ntyagi87@users.noreply.github.com>
2022-04-05 13:54:36 +0530

    Updated contact form

--- src/pages/ContactUs/Contact.jsx
+++ src/pages/ContactUs/Contact.jsx
@@ -1,40 +1,45 @@
-import react from 'react';
+import React, { useState } from "react";
 
-const Contact = () => <div>
-<div data-mesh-id="comp-l1dospg0inlineContent" data-testid="inline-content" class="">
-    <div data-mesh-id="comp-l1dospg0inlineContent-gridContainer" data-testid="mesh-container-content">
-        <form id="comp-l1dospgh" class="yBJuM">
-            <div data-mesh-id="comp-l1dospghinlineContent" data-testid="inline-content" class="">
-                <div data-mesh-id="comp-l1dospghinlineContent-gridContainer" data-testid="mesh-container-content">
-                    <div id="comp-l1dosphl" class="_2dBhC _2nVk2 _65cjg _3TyBu">
-                        <label for="input_comp-l1dosphl" class="aHD7c">Name</label>
-                        <div class="XRJUI"><input name="name" id="input_comp-l1dosphl" class="_1SOvY has-custom-focus" type="text" placeholder="Enter your name" required="" maxlength="100" value="" /></div>
-                    </div>
-                    <div id="comp-l1dospi51" class="_2dBhC _2nVk2 _65cjg">
-                        <label for="input_comp-l1dospi51" class="aHD7c">Email</label>
-                        <div class="XRJUI">
-                            <input name="email" id="input_comp-l1dospi51" class="_1SOvY has-custom-focus" type="email" placeholder="Enter your email" required="" pattern="^.+@.+\.[a-zA-Z]{2,63}$" maxlength="250" value="" />
-                        </div>
-                    </div>
-                    <div id="comp-l1dospi91" class="_2dBhC _2nVk2">
-                        <label for="input_comp-l1dospi91" class="aHD7c">Subject</label>
-                        <div class="XRJUI"><input name="subject" id="input_comp-l1dospi91" class="_1SOvY has-custom-focus" type="text" placeholder="Type the subject" value="" /></div>
-                    </div>
-                    <div id="comp-l1dospid" class="bItEI _1mQNr">
-                        <label for="textarea_comp-l1dospid" class="_20uhs">Message</label><textarea id="textarea_comp-l1dospid" class="_1VWbH has-custom-focus" placeholder="Type your message here..."></textarea>
-                    </div>
-                    <div id="comp-l1dospir" aria-disabled="false" class="_2UgQw">
-                        <button aria-disabled="false" data-testid="buttonElement" class="_1fbEI"><span class="_1Qjd7">Submit</span></button>
-                    </div>
-                    <div id="comp-l1dospj1" class="_1Q9if" data-testid="richTextElement">
-                        <p class="font_8" ><span class="color_15">Thanks for submitting!</span></p>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
-</div>
+const FORM_ENDPOINT = ""; // TODO - fill on the later step
 
-                        
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <h2>Thank you!</h2>
+        <div>We'll be in touch soon.</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div>
+        <input type="text" placeholder="Your name" name="name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" name="email" required />
+      </div>
+      <div>
+        <textarea placeholder="Your message" name="message" required />
+      </div>
+      <div>
+        <button type="submit"> Send a message </button>
+      </div>
+    </form>
+  );
+};
+
 export default Contact;
